e9a630f04198787120dabbd6a1e68027db465eb6
Aaron-Uriel <40283192+Aaron-Uriel@users.noreply.github.com>
2018-10-04 10:51:51 -0500

    Añado el programa XD

---
+++ adivina numeros aleatorios.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+    srand(time(0));
+    int intento;
+    int correcto;
+    int x = 5;
+    correcto = 1 + (rand() % 15);
+    while (intento != correcto)
+    {
+        cout << "Adivina el numero" << endl;
+        cin >> intento;
+        if (intento != correcto)
+        {
+            cout << "Incorrecto" << endl;
+            x--;
+            if (x == 0)
+            {
+                cout << "Perdiste!" << endl;
+                return 0;
+            }
+            cout << "Te quedan " << x << " intentos" << endl;
+        }
+        if (intento == correcto)
+        {
+            cout << "Felicidades, ganaste NADA!" << endl;
+        }
+    }
+    return 0;
+}
